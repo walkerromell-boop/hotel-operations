5c2944a1244d13b6250c05f3ddf12a0c7a3e1451
Romell Walker <walkerromell@gmail.com>
2025-10-21 14:28:08 -0400

    derived methods booked room

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -11,8 +11,8 @@ public class Hotel {
         this.name = name;
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
-        this.bookedBasicRooms=0;
-        this.bookedSuites=0;
+        this.bookedBasicRooms = 0;
+        this.bookedSuites = 0;
     }
 
     public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
@@ -21,6 +21,22 @@ public class Hotel {
         this.numberOfRooms = numberOfRooms;
         this.bookedSuites = bookedSuites;
         this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public int getAvailbleRooms() {
+        return numberOfRooms - bookedBasicRooms;
+    }
+
+    public int getAvailbleSuites() {
+        return numberOfSuites - bookedSuites;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (getAvailbleRooms() >= numberOfRooms) {
+            bookedBasicRooms += numberOfRooms;
+            return true;
+        }else {return false;
+        }
     }
 
 
